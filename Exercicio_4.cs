9178ff7acd9b5157e21d599973a2390d4a81ee61
Gabriela <gabrielamatheus873@gmail.com>
2022-12-21 09:57:58 -0300

    exercicio 4 percentage of volume

---
+++ Exercicio_4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace QuantidadePOV
+{
+    class Exercicio_4
+    {
+        public static int funcaoRetornaQuantidadePOV(decimal porcentagem, int totalNegociado)
+        {
+            return (int) Math.Round(((porcentagem + 1) * totalNegociado) * porcentagem);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Exercicio_4.funcaoRetornaQuantidadePOV(0.1m, 90));
+            Console.WriteLine(Exercicio_4.funcaoRetornaQuantidadePOV(0.1m, 100));
+            Console.WriteLine(Exercicio_4.funcaoRetornaQuantidadePOV(0.2m, 70));
+        }
+    }
+}
